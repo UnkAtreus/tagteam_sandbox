657e1896ad00d29256f6ae561223bb13bbe2879e
UnkAtreus <nom_noon00548@hotmail.com>
2021-03-19 06:30:46 +0700

    Updates

---
+++ precache-manifest.e8176647119377fb0b2c892ef1f58958.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57c246a513688ca479c",
+    "url": "/tagteam_sandbox/static/js/main.61a20a5a.chunk.js"
+  },
+  {
+    "revision": "4fa3269ab86af93c0840",
+    "url": "/tagteam_sandbox/static/js/runtime~main.670690a4.js"
+  },
+  {
+    "revision": "e1043ca32b1ab901abf8",
+    "url": "/tagteam_sandbox/static/css/2.0011913d.chunk.css"
+  },
+  {
+    "revision": "e1043ca32b1ab901abf8",
+    "url": "/tagteam_sandbox/static/js/2.9b872c67.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tagteam_sandbox/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tagteam_sandbox/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tagteam_sandbox/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tagteam_sandbox/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "751572cd845246de0d2c20ab26ade480",
+    "url": "/tagteam_sandbox/static/media/sample-content.751572cd.jpg"
+  },
+  {
+    "revision": "0992c2a1dc6244936340626ef1c5904b",
+    "url": "/tagteam_sandbox/static/media/sample-icon.0992c2a1.svg"
+  },
+  {
+    "revision": "97839c89320fad68c523df566a7ff731",
+    "url": "/tagteam_sandbox/static/media/sample-logo.97839c89.png"
+  },
+  {
+    "revision": "0d3f391f769171f7dfa2f4f0275fa227",
+    "url": "/tagteam_sandbox/static/media/sample-logo.0d3f391f.svg"
+  },
+  {
+    "revision": "667bc1fad5cd9a81a545cffce71bdc51",
+    "url": "/tagteam_sandbox/static/media/sample-video.667bc1fa.mp4"
+  },
+  {
+    "revision": "0eb3f36bd46c4fb32b61148122b60f7b",
+    "url": "/tagteam_sandbox/static/media/sample-audio.0eb3f36b.mp3"
+  },
+  {
+    "revision": "ce355ba2c542f777bbe4e220bd18d1df",
+    "url": "/tagteam_sandbox/static/media/sample-document.ce355ba2.pdf"
+  },
+  {
+    "revision": "9e63b567d1bc7e5cf5b599ee4d9aab39",
+    "url": "/tagteam_sandbox/index.html"
+  }
+];
